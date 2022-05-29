7da4075b211cc7f9b623af304ffe3f8795e5a4a3
Alexander Baker Vallot <vallotalexander@gmail.com>
2022-05-29 22:50:52 +0100

    began removing dependencies

--- src/TypescriptSourceFiles/Operation.ts
+++ src/TypescriptSourceFiles/Operation.ts
@@ -4,11 +4,12 @@ export class Operation {
     private typesense = require("typesense");
     private settings = require(this.h + "/.typesense-cli/typesense-cli.config.json");
     private node;
-    private client;
+    public client;
     private start_time: Date;
     private finish_time: Date;
     
-    constructor() {
+    constructor(...args) {
+        console.log(args);
         this.node = this.settings.serverNode;                   // typesense server node specified in the config file
         this.client = new this.typesense.Client(this.node);     // client to be used for process
         if(this.client == null || this.client == undefined){
--- src/TypescriptSourceFiles/test.js
+++
@@ -1,3 +0,0 @@
-let imp = require("./Operation.js");
-let op = new imp.Operation("world");
-op.test(1000000000);
