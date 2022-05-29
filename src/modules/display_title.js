6f50b8f1d3829d89522bf1570ca1d739822aeb88
Alexander Baker Vallot <vallotalexander@gmail.com>
2022-05-29 08:04:49 +0100

    begining to refactor code

--- src/modules/display_title.js
+++
@@ -1,9 +0,0 @@
-var chalk = require("chalk");
-var clear = require("clear");
-var figlet = require("figlet");
-module.exports = {
-    title: function () {
-        clear();
-        console.log(chalk.red(figlet.textSync("TYPESENSE-CLI", { horizontalLayout: "full" })));
-    },
-};
